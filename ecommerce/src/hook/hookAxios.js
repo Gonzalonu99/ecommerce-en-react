211ecd73249ed7f25165c853ee1ca5a55a05546e
Gonzalo Núñez <gonzalonu99@gmail.com>
2023-04-21 11:33:11 -0300

    feat hook axios

---
+++ ecommerce/src/hook/hookAxios.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react"
+
+const useAxios = (url)=>{
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState({
+        isError: false,
+        message:""
+    })
+
+    useEffect(()=>{
+        const getData = async (url)=>{
+            setIsLoading(true);
+            try {
+                const res = await axios.get(url);
+                setData(Object.values(res.data));
+            } catch (err) {
+                setError({
+                    isError:true,
+                    message:err.message ||"There was an error"
+                })
+            }
+            setTimeout(()=> setIsLoading(false), 500);
+        };
+        getData(url);
+    }, [url]);
+    return {data, isLoading, error}
+}
+
+export default useAxios;
