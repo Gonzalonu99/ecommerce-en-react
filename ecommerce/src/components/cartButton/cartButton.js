f7c37e4922ebaf8405879046804a5ab63a599172
Gonzalo Núñez <gonzalonu99@gmail.com>
2023-04-26 20:24:57 -0300

    feat: addToCart button

--- ecommerce/src/components/cartButton/cartButton.js
+++ ecommerce/src/components/cartButton/cartButton.js
@@ -3,10 +3,10 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import ShoppingCart from '@mui/icons-material/ShoppingCart'
 
-export default function AddToCart() {
+export default function AddToCart(props) {
   return (
     <Stack style={{position:'relative', marginBottom:'5px', width:'200px'}}>
-      <Button variant="outlined" style={{ fontSize:'15px', padding:'8px', borderRadius:'15px'}}>
+      <Button variant="outlined" style={{ fontSize:'15px', padding:'8px', borderRadius:'15px'}} onClick={props.onClick}>
         <ShoppingCart style={{marginRight:'5px'}}/>
         Agregar
       </Button>
