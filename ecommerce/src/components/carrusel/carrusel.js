bc07592be4b4760372f1a0fe38cb75652676053d
Gonzalo Núñez <gonzalonu99@gmail.com>
2023-04-28 16:37:17 -0300

    feat: fetch data

--- ecommerce/src/components/carrusel/carrusel.js
+++ ecommerce/src/components/carrusel/carrusel.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import useAxios from '../../hook/hookAxios';
+import React, {useState, useEffect} from 'react';
 import ProductCard from './card/cards';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 
 const ProductsCarousel = () => {
-    const {data, isLoading, error}= useAxios("http://a365.com.ar/ecommerce/getProductos");
-
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+  
+    useEffect(() => {
+      fetch("http://a365.com.ar/ecommerce/getProductos")
+        .then((response) => response.json())
+        .then((data) => {
+          setData(data);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          setError(error);
+          setIsLoading(false);
+        });
+    }, []);
+    console.log(data);
     const responsive = {
         0:{items:2},
         768:{items:3},
         1024:{items:4},
     };
+
+
     if (isLoading){return <div>Loading...</div>}
-    if(error.isError){return <div>{error.message}</div>}
+    if (error && error.isError) {
+        return <div>{error.message}</div>;
+      }
   return (
     <>
-        {Object.entries(data).map(([category, products])=>(
-            <div key={products.id} className='products-carousel-container'>
-                <h2 className="products-carousel-title">{category}</h2>
+        {data.SubRubros.map((subrubro)=>(
+            <div key={subrubro.NombreSubRubro} className='products-carousel-container'>
+                <h2 className="products-carousel-title">{subrubro.NombreSubRubro}</h2>
                 <AliceCarousel
                     touchMoveDefaultEvents={true}
                     mouseTracking
@@ -26,7 +44,7 @@ const ProductsCarousel = () => {
                     autoPlay
                     autoPlayInterval={3000}
                     responsive={responsive}
-                    items={products.map((product)=>(
+                    items={subrubro.Productos.map((product)=>(
                         <ProductCard 
                             id={product.id}
                             name={product.name}
