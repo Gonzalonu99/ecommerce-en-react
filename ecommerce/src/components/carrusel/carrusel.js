930c2cd813277b47cf7c82d6ad4eb4b1b9174b31
Gonzalo Núñez <gonzalonu99@gmail.com>
2023-04-30 20:47:15 -0300

    feat: react-slick

--- ecommerce/src/components/carrusel/carrusel.js
+++ ecommerce/src/components/carrusel/carrusel.js
@@ -1,7 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import ProductCard from './card/cards';
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const ProductsCarousel = () => {
     const [data, setData] = useState(null);
@@ -21,12 +22,42 @@ const ProductsCarousel = () => {
         });
     }, []);
     console.log(data);
-    const responsive = {
-        0:{items:2},
-        768:{items:3},
-        1024:{items:4},
-    };
 
+    const settings = {
+      dots: true,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      initialSlide: 0,
+      infinite: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    };
 
     if (isLoading){return <div>Loading...</div>}
     if (error && error.isError) {
@@ -37,14 +68,8 @@ const ProductsCarousel = () => {
         {data.SubRubros.map((subrubro)=>(
             <div key={subrubro.NombreSubRubro} className='products-carousel-container'>
                 <h2 className="products-carousel-title">{subrubro.NombreSubRubro}</h2>
-                <AliceCarousel
-                    touchMoveDefaultEvents={true}
-                    mouseTracking
-                    infinite
-                    autoPlay
-                    autoPlayInterval={3000}
-                    responsive={responsive}
-                    items={subrubro.Productos.map((product)=>(
+                <Slider {...settings}>
+                {subrubro.Productos.map((product)=>(
                         <ProductCard 
                             id={product.id}
                             name={product.name}
@@ -53,7 +78,7 @@ const ProductsCarousel = () => {
                             price={product.price}
                         />
                     ))}
-                />
+                </Slider>
             </div>
         ))}
     </>
