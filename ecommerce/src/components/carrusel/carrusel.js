5ddf34bae2e05c37990617ef6b73cb43f6742445
Gonzalo Núñez <gonzalonu99@gmail.com>
2023-05-02 18:23:32 -0300

    fix

--- ecommerce/src/components/carrusel/carrusel.js
+++ ecommerce/src/components/carrusel/carrusel.js
@@ -27,31 +27,30 @@ const ProductsCarousel = () => {
     dots: true,
     infinite: false,
     speed: 500,
-    slidesToShow: 5,
+    slidesToShow: 3,
     slidesToScroll: 1,
     initialSlide: 1,
     responsive: [
       {
-        breakpoint: 1250,
+        breakpoint: 1024,
         settings: {
-          slidesToShow: 5,
-          slidesToScroll: 0.5,
-          dots: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
           initialSlide: 1
         }
       },
       {
         breakpoint: 768,
         settings: {
-          slidesToShow: 2.5,
-          slidesToScroll: 0.5,
+          slidesToShow: 1,
+          slidesToScroll: 2,
           initialSlide: 1
         }
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 1.1,       
+          slidesToShow: 1.1,
           slidesToScroll: 0.92,
           initialSlide: 1
         }
@@ -71,6 +70,7 @@ const ProductsCarousel = () => {
           <Slider {...settings}>
             {subrubro.Productos.map((product)=>(
               <ProductCard 
+                key={product.id}
                 id={product.id}
                 name={product.name}
                 image={product.image}
