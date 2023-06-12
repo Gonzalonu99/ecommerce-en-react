b8460b05ede2da87ad984a8be13eef61212388d5
Alan Opresnik <48458984+porstitus@users.noreply.github.com>
2023-06-12 22:28:25 +0200

    Feat/footer (#6)
    * fix scrollToCategory
    
    * feat and style: footer
    
    Co-authored-by: Gonzalo Núñez <Gonzalonu99@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Gonzalo Núñez <Gonzalonu99@users.noreply.github.com>
    Co-authored-by: Gonzalo Núñez <92396308+Gonzalonu99@users.noreply.github.com>

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,15 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/navbar/index";
 import ProductsCarousel from "./components/carrusel/carrusel";
+import footer1 from "./components/footer/footer";
 import { CartProvider } from "./hook/useCart";
 import { useState, useEffect } from "react";
 import jwt_decode from "jwt-decode";
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Footer from "./components/footer/footer";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showFooter, setShowFooter] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowFooter(true);
+    }, 2000);
+  }, []);
+
   useEffect(() => {
     // Verificar si hay un token almacenado en el localStorage al cargar la aplicación
     const token = localStorage.getItem("token");
@@ -37,18 +46,17 @@ function App() {
         console.log(decodedToken);
         // Almacenar el token JWT en el almacenamiento local (localStorage)
         localStorage.setItem("token", token);
-        const usuarioId = data.usuarioId
+        const usuarioId = data.usuarioId;
         localStorage.setItem("usuarioId", usuarioId);
         setIsLoggedIn(true);
         console.log("Inicio de sesión exitoso");
         console.log(data);
         if (data.success === true) {
-          toast.success('Inicio de sesión exitoso',{
-            className: 'mobile-toast',
+          toast.success("Inicio de sesión exitoso", {
+            className: "mobile-toast",
           });
-        }/* aca borre el else xq no estaba funcionando el toast, asi que agarre el toast y lo meti en la linea 60  */
-          /*donde funciona bien */
-        
+        } /* aca borre el else xq no estaba funcionando el toast, asi que agarre el toast y lo meti en la linea 60  */
+        /*donde funciona bien */
       } else {
         // El inicio de sesión falló, muestra un mensaje de error
         const data = await response.json();
@@ -57,25 +65,24 @@ function App() {
       }
     } catch (error) {
       console.error("Error al iniciar sesión:", error.message);
-        toast.error('Inicio de sesión fallido',{
-          className: 'mobile-toast'
-      })        
+      toast.error("Inicio de sesión fallido", {
+        className: "mobile-toast",
+      });
     }
   };
   const handleLogout = () => {
     // Eliminar el token JWT del almacenamiento local (localStorage)
     localStorage.clear();
-    localStorage.removeItem('usuarioId');
+    localStorage.removeItem("usuarioId");
     localStorage.removeItem("token");
     setIsLoggedIn(false);
-    toast.info('Cerraste sesión',{
-      className: 'mobile-toast',
+    toast.info("Cerraste sesión", {
+      className: "mobile-toast",
     });
   };
   const scrollToCategory = (categoryId) => {
     const element = document.getElementById(categoryId);
-    const offset = 130; // Ajusta el valor según tus necesidades
-  
+    const offset = 130;
     setTimeout(() => {
       const topPos = element.offsetTop - offset;
       window.scrollTo({ top: topPos, behavior: "smooth" });
@@ -90,7 +97,8 @@ function App() {
         handleLogout={handleLogout}
       />
       <ProductsCarousel />
-      <ToastContainer/>
+      <ToastContainer />
+      {showFooter && <Footer />}
     </CartProvider>
   );
 }
