a7692ddc7db13c5f887ac597d7749f1f1d544959
Gonzalo Núñez <92396308+Gonzalonu99@users.noreply.github.com>
2023-06-13 00:41:33 -0300

    Feat/favContext (#9)
    * feat: favContext and drawer
    
    * feat favDrawer actualización automática
    
    * fix
    
    * style: heart icon navbar

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -3,16 +3,18 @@ import Navbar from "./components/navbar/index";
 import ProductsCarousel from "./components/carrusel/carrusel";
 import footer1 from "./components/footer/footer";
 import { CartProvider } from "./hook/useCart";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext, useRef } from "react";
 import jwt_decode from "jwt-decode";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/footer/footer";
+import { FavoritesContext, FavoritesProvider } from "./hook/useFav";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [showFooter, setShowFooter] = useState(false);
-
+  const {getFavProduct} = useContext(FavoritesContext);
+  const favoritesProviderRef = useRef();
   useEffect(() => {
     setTimeout(() => {
       setShowFooter(true);
@@ -51,6 +53,7 @@ function App() {
         setIsLoggedIn(true);
         console.log("Inicio de sesión exitoso");
         console.log(data);
+        getFavProduct();
         if (data.success === true) {
           toast.success("Inicio de sesión exitoso", {
             className: "mobile-toast",
@@ -89,17 +92,19 @@ function App() {
     }, 100);
   };
   return (
-    <CartProvider>
-      <Navbar
-        scrollToCategory={scrollToCategory}
-        isLoggedIn={isLoggedIn}
-        handleLogin={handleLogin}
-        handleLogout={handleLogout}
-      />
-      <ProductsCarousel />
-      <ToastContainer />
-      {showFooter && <Footer />}
-    </CartProvider>
+    <FavoritesProvider isLoggedIn={isLoggedIn} ref={favoritesProviderRef}>
+      <CartProvider>
+        <Navbar
+          scrollToCategory={scrollToCategory}
+          isLoggedIn={isLoggedIn}
+          handleLogin={handleLogin}
+          handleLogout={handleLogout}
+        />
+        <ProductsCarousel />
+        <ToastContainer />
+        {showFooter && <Footer />}
+      </CartProvider>
+    </FavoritesProvider>
   );
 }
 
