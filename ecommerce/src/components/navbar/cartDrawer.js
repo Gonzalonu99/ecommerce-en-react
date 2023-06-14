8c459261e173d5991ed4e5d0bc977ef19d99370f
Gonzalo Núñez <gonzalonu99@gmail.com>
2023-06-14 16:44:10 -0300

    fix

--- ecommerce/src/components/navbar/cartDrawer.js
+++ ecommerce/src/components/navbar/cartDrawer.js
@@ -33,7 +33,7 @@ function CartDrawer(props) {
     removeFromCartAtOnce,
     getTotalPrice,
     addToCart,
-    emptyCart
+    emptyCart,
   } = useCart();
   const [openDialog, setOpenDialog] = useState(false);
   const [emptyModal, setEmptyModal] = useState(false);
@@ -86,29 +86,34 @@ function CartDrawer(props) {
   };
   return (
     <React.Fragment>
-      <IconButton style={{ position: 'absolute', zIndex:'2000', top: 0, right: 0 }} onClick={props.handleDrawerCart}>
-        <Close/>
+      <IconButton
+        style={{ position: "absolute", zIndex: "2000", top: 0, right: 0 }}
+        onClick={props.handleDrawerCart}
+      >
+        <Close />
       </IconButton>
-      <List dense sx={{ padding: '16px' }}>
-      
-        {groupedItems.length > 0  ? (
+      <List dense sx={{ padding: "16px" }}>
+        {groupedItems.length > 0 ? (
           groupedItems.map((item) => (
             <ListItem key={item.id}>
               <ListItemAvatar>
-                <img className='img-cart-drawer-product'  src={item.imagen}/>
+                <img className="img-cart-drawer-product" src={item.imagen} />
               </ListItemAvatar>
-              <ListItemText className='text-cart-drawer-product'
+              <ListItemText
+                className="text-cart-drawer-product"
                 primary={`${item.name}`}
-                secondary={`$${item.price*item.quantity}`} 
+                secondary={`$${item.price * item.quantity}`}
               />
               <ListItemSecondaryAction>
-                <IconButton style={{marginRight:"0px"}}
-                  edge="end" aria-label="delete" onClick={() => handleRemoveClick(item)}>
-                  <RemoveCircleOutline/>
+                <IconButton
+                  style={{ marginRight: "0px" }}
+                  edge="end"
+                  aria-label="delete"
+                  onClick={() => handleRemoveClick(item)}
+                >
+                  <RemoveCircleOutline />
                 </IconButton>
-                <>
-                  {item.quantity}
-                </>
+                <>{item.quantity}</>
                 <IconButton
                   edge="end"
                   aria-label="delete"
@@ -118,9 +123,7 @@ function CartDrawer(props) {
                 </IconButton>
               </ListItemSecondaryAction>
             </ListItem>
-            
           ))
-
         ) : (
           <Box>
             <Typography variant="subtitle1" align="center">
@@ -135,42 +138,49 @@ function CartDrawer(props) {
             </Button>
           </Box>
         )}
-        <List sx={{ padding: '16px' }}>
-        <button className='empty-cart-drawer'
-          onClick={emptyCart}>
-            Vaciar carrito
-        </button>
       </List>
-      </List>
-      
-      <Divider />
-
       {groupedItems.length > 0 && (
         <>
-      <List sx={{ padding: '16px' }}>
-        <ListItem>
-          <ListItemText primary="Total" />
-          <Typography 
-            variant="subtitle1" component="span">
-              ${getTotalPrice()}
-          </Typography>
-        </ListItem>
-      </List>
-      <Divider />
-      
-      <Dialog className='alert-cart' open={openDialog} onClose={handleClose}>
-      <DialogTitle className='alert-tittle-cart'>ATENCIÓN</DialogTitle>
-      <DialogContent>
-        <DialogContentText className='subtittle-cart-clear'>
-          ¿Desea eliminar el producto del carrito?
-        </DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <Button className='btn-cart-clear-cancell' onClick={handleClose}>Cancelar</Button>
-        <Button className='btn-cart-clear-remove' onClick={handleRemoveConfirm} color="error">Quitar</Button>
-      </DialogActions>
-    </Dialog>
-     <Dialog
+          <List sx={{ padding: "16px" }}>
+            <ListItem sx={{ padding: "16px" }}>
+              <button className="empty-cart-drawer" onClick={handleEmptyCartModal}>
+                Vaciar carrito
+              </button>
+            </ListItem>
+            <Divider/>
+            <ListItem>
+              <ListItemText primary="Total" />
+              <Typography variant="subtitle1" component="span">
+                ${getTotalPrice()}
+              </Typography>
+            </ListItem>
+          </List>
+          <Divider />
+          <Dialog
+            className="alert-cart"
+            open={openDialog}
+            onClose={handleClose}
+          >
+            <DialogTitle className="alert-tittle-cart">ATENCIÓN</DialogTitle>
+            <DialogContent>
+              <DialogContentText className="subtittle-cart-clear">
+                ¿Desea eliminar el producto del carrito?
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button className="btn-cart-clear-cancell" onClick={handleClose}>
+                Cancelar
+              </Button>
+              <Button
+                className="btn-cart-clear-remove"
+                onClick={handleRemoveConfirm}
+                color="error"
+              >
+                Quitar
+              </Button>
+            </DialogActions>
+          </Dialog>
+          <Dialog
             className="alert-cart"
             open={emptyModal}
             onClose={handleClose}
