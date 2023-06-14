0319874e75d2981f88f7830ec2532baba7bced47
Gonzalo Núñez <gonzalonu99@gmail.com>
2023-06-14 19:40:58 -0300

    feat: user name in navbar

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -12,9 +12,9 @@ import WspLogo from "./components/wspLogo/wspLogo";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userData, setUserData] = useState({});
   const [showFooter, setShowFooter] = useState(false);
   const {getFavProduct} = useContext(FavoritesContext);
-
   useEffect(() => {
     setTimeout(() => {
       setShowFooter(true);
@@ -37,22 +37,18 @@ function App() {
         },
         body: JSON.stringify({ Email: email, Password: password }),
       });
-      console.log(response);
       if (response.status === 200) {
         const data = await response.json();
         // El inicio de sesión fue exitoso, puedes hacer lo que quieras aquí
         // Obtener el token JWT de la respuesta
         const token = data.token;
-        // Decodificar el token para obtener la información del usuario
-        const decodedToken = jwt_decode(token);
-        console.log(decodedToken);
         // Almacenar el token JWT en el almacenamiento local (localStorage)
         localStorage.setItem("token", token);
         const usuarioId = data.usuarioId;
         localStorage.setItem("usuarioId", usuarioId);
         setIsLoggedIn(true);
         console.log("Inicio de sesión exitoso");
-        console.log(data);
+        setUserData(data);
         getFavProduct();
         if (data.success === true) {
           toast.success("Inicio de sesión exitoso", {
@@ -92,11 +88,12 @@ function App() {
     }, 100);
   };
   return (
-    <FavoritesProvider isLoggedIn={isLoggedIn} >
+    <FavoritesProvider isLoggedIn={isLoggedIn}>
       <CartProvider>
         <Navbar
           scrollToCategory={scrollToCategory}
           isLoggedIn={isLoggedIn}
+          userData={userData}
           handleLogin={handleLogin}
           handleLogout={handleLogout}
         />
