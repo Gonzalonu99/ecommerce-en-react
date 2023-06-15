a03d567c6985dd4091e9c0f0ae3385348a6c8ee2
Gonzalo Núñez <gonzalonu99@gmail.com>
2023-06-15 00:08:59 -0300

    feat cart-icon-favDrawer

--- ecommerce/src/components/navbar/favDrawer.js
+++ ecommerce/src/components/navbar/favDrawer.js
@@ -12,18 +12,19 @@ import {
 } from "@mui/material";
 import React, { useContext, useEffect } from "react";
 import { FavoritesContext } from "../../hook/useFav";
-import './favDrawer.css';
-import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import "./favDrawer.css";
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+import AddShoppingCartOutlinedIcon from '@mui/icons-material/AddShoppingCartOutlined';
 const FavDrawer = (props) => {
   const { favData, handleFavorites } = useContext(FavoritesContext);
-  const handleDelete = async (id, precioId)=>{
+  const handleDelete = async (id, precioId) => {
     await handleFavorites(id, precioId);
-  }
+  };
   return (
     <React.Fragment>
-    <>
-    <p className="tittle-fav-drawer">Mis Favoritos</p>
-    </>
+      <>
+        <p className="tittle-fav-drawer">Mis Favoritos</p>
+      </>
       <IconButton
         style={{ position: "absolute", zIndex: "2000", top: 0, right: 0 }}
         onClick={props.handleFavDrawer}
@@ -35,20 +36,34 @@ const FavDrawer = (props) => {
           return (
             <ListItem className="list-drawer-fav" key={index} id={item.Id}>
               <ListItemAvatar>
-                <img className="img-drawer-fav" src={item.Imagen} alt={item.Nombre} />
+                <img
+                  className="img-drawer-fav"
+                  src={item.Imagen}
+                  alt={item.Nombre}
+                />
               </ListItemAvatar>
-             
+
               <ListItemText primary={item.Nombre} />
-              <ListItemSecondaryAction>
-                <IconButton edge="end" aria-label="delete" onClick={()=>handleDelete(item.Id, item.PrecioId)}>
-                  <DeleteOutlineOutlinedIcon/>
-                </IconButton>
-              </ListItemSecondaryAction>
+                <ListItemSecondaryAction>
+                  <IconButton edge='end' aria-label="cart" className="fd-cart-icon">
+                    <AddShoppingCartOutlinedIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+                <ListItemSecondaryAction>
+                  <IconButton
+                    className="fd-trash-icon"
+                    edge="end"
+                    aria-label="delete"
+                    onClick={() => handleDelete(item.Id, item.PrecioId)}
+                  >
+                    <DeleteOutlineOutlinedIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
             </ListItem>
           );
         })
       ) : (
-          <Typography >Agregar al carrito</Typography>      
+        <Typography>Agregar al carrito</Typography>
       )}
     </React.Fragment>
   );
