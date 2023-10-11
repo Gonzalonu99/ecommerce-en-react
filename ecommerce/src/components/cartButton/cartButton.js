2321b17f120b76ece8bc299c8a3b22d922443c05
Alan Opresnik <48458984+AlanOpresnik@users.noreply.github.com>
2023-10-10 22:35:19 -0300

    cambie estilos de las cards e implemente un modal (#23)
    Co-authored-by: Alan Opresnik <alanopresnik@MacBook-Air-de-Alan.local>

--- ecommerce/src/components/cartButton/cartButton.js
+++ ecommerce/src/components/cartButton/cartButton.js
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import "../../components/carrusel/card/cards.css"
 import ShoppingCart from '@mui/icons-material/ShoppingCart'
 
 export default function AddToCart(props) {
   return (
     <Stack style={{position:'relative'}}>
-      <Button variant="outlined" style={{ fontSize:'15px', borderRadius:'25px', color:'#f4e2d0', borderColor:'#f4e2d0', backgroundColor:"#111", width: 'fit-content'}} onClick={props.onClick}>
-        <ShoppingCart/>
+      <Button variant="outlined" className='btn-card-cart' style={{ fontSize:'15px', borderRadius:'10px', color:'#fff', backgroundColor:"#111", width: '10rem'}} onClick={props.onClick}>
+        
+          AGREGAR
       </Button>
     </Stack>
   );
