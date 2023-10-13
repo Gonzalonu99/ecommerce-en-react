006bf445b5eb1fae02e5db525ef75c784577b40b
Gonzalo Núñez <gonzalonu99@gmail.com>
2023-10-13 11:23:55 -0300

    refactor and style

--- ecommerce/src/components/redesDivider/redes.js
+++ ecommerce/src/components/redesDivider/redes.js
@@ -1,23 +1,32 @@
-import React from 'react'
+import React from "react";
 import fb from "../../img/facebookLogo.webp";
 import instagram from "../../img/instagramLogo.webp";
-import "./redes.css"
+import "./redes.css";
 const Redes = () => {
   return (
-    <div className='redes-container'>
-        <div className='redes-header'>
-            <h4><span>Seguinos</span> en nuestras redes para mantenerte actualizado</h4>
-        </div>
-        <div className='redes-social'>
-            <a>
-                <img className='redes-img-social' src={fb} alt='iMAGEN FACEBOOK'/>
+    <div className="redes-container">
+      <div className="redes-section-padding">
+        <div className="inline-redes-section">
+          <h2 className="h2-heading centered">
+            <strong className="bold-redes-text">Seguinos</strong> en nuestras
+            redes para mantenerte actualizado.
+          </h2>
+          <div className="redes-icons">
+            <a href="" className="ri-inline-block" target="_blank">
+              <img src={fb} alt="redes-icon-fb" className="redes-icon" />
             </a>
-            <a>
-                <img className='redes-img-social' src={instagram} alt='INSTAGRAM'/>
+            <a href="" className="ri-inline-block" target="_blank">
+              <img
+                src={instagram}
+                alt="redes-icon-instagram"
+                className="redes-icon"
+              />
             </a>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Redes
+export default Redes;
