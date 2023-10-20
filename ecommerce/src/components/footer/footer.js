0b900ba7efffb77aaf508237d88667fc8332d788
Gonzalo Núñez <gonzalonu99@gmail.com>
2023-10-20 13:20:25 -0300

    feat and style

--- ecommerce/src/components/footer/footer.js
+++ ecommerce/src/components/footer/footer.js
@@ -1,94 +1,122 @@
 import React from "react";
 import "../footer/footer.css";
-import fb from "../../img/facebookLogo.webp";
-import twitter from "../../img/twitterLogo.webp";
-import instagram from "../../img/instagramLogo.webp";
-import {Link} from "react-router-dom"
+import facebook from "../../img/facebookFooter.webp";
+import instagram from "../../img/instagramFooter.webp";
+import { Link } from "react-router-dom";
+import Logo from "../../img/pizza_grosso.webp";
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="sb__footer section__padding">
-        <div className="sb__footer-links">
-          <div className="sb__footer-links-div">
-            <h4>Para empresas</h4>
-            <a href="#">
-              <p>Empleador</p>
-            </a>
-            <a href="#">
-              <p>Plan de salud</p>
-            </a>
-            <a href="#">
-              <p>Individuos</p>
-            </a>
-          </div>
-          <div className="sb__footer-links-div">
-            <h4>Recursos</h4>
-            <a href="#">
-              <p>Centro de recursos</p>
-            </a>
-            <a href="#">
-              <p>Testimonios</p>
-            </a>
-            <a href="#">
-              <p>STV</p>
-            </a>
-          </div>
-          <div className="sb__footer-links-div">
-            <h4>Partners</h4>
-            <a href="#">
-              <p>Swing tech</p>
-            </a>
-          </div>
-          <div className="sb__footer-links-div">
-            <h4>Compañía</h4>
-            <Link to="/nosotros">Nosotros</Link>
-            <Link to="/contacto">Contacto</Link>
-          </div>
-          <div className="sb__footer-links-div">
-            <h4>Enlaces</h4>
-            <a href="">
-              <div>
-                <p>Términos y condiciones</p>
+      <div className="section-padding footer">
+        <div className="footer-wrapper">
+          <div className="footer-top-wrapper">
+            <div className="footer-menu-wrapper">
+              <img
+                src={Logo}
+                alt="Grosso Logo"
+                className="footer-logo w-inline-block"
+              />
+              <div className="footer-menu-block">
+                <div className="links-menu-block">
+                  <Link to="/" className="footer-link w-inline-block">
+                    Menu
+                  </Link>
+                  <Link to="/ubicacion" className="footer-link w-inline-block">
+                    Ubicación
+                  </Link>
+                </div>
               </div>
-            </a>
-            <a href="">
-              <div>
-                <p>Privacidad</p>
+              <div className="footer-menu-block">
+                <div className="links-menu-block">
+                  <Link to="/nosotros" className="footer-link w-inline-block">
+                    Nosotros
+                  </Link>
+                  <Link to="/contacto" className="footer-link w-inline-block">
+                    Contacto
+                  </Link>
+                </div>
               </div>
-            </a>
-            <a href="">
-              <div>
-                <p>Seguridad</p>
+              <div className="footer-menu-block">
+                <div className="footer-info-block">
+                  <div className="footer-title">Horarios</div>
+                  <div className="info-wrapper hours">
+                    <div className="footer-info">
+                      <div className="footer-bold-text">Lunes - Jueves</div>
+                      <div class="footer-text">10 a.m. - 10 p.m.</div>
+                    </div>
+                    <div className="footer-info">
+                      <div className="footer-bold-text">Viernes - Sábado</div>
+                      <div class="footer-text">10 a.m. - 11 p.m.</div>
+                    </div>
+                  </div>
+                </div>
               </div>
-            </a>
-            <a href="">
-              <div>
-                <p>Cookies</p>
+              <div className="footer-menu-block location">
+                <div className="footer-info-block">
+                  <div className="footer-title">Visitanos</div>
+                  <div className="footer-info-wrapper">
+                    <div className="footer-info">
+                      <div className="footer-bold-text">
+                        Ing. Quartino 2176, B1714 Ituzaingó, Provincia de Buenos
+                        Aires.
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-            </a>
-          </div>
-          <div className="sb__footer-links-div">
-            <h4>Próximamente</h4>
-            <div className="social-media">
-              <p>
-                <img src={fb} alt="Logo de Facebook"></img>
-              </p>
-              <p>
-                <img src={twitter} alt="Logo de Twitter"></img>
-              </p>
-              <p>
-                <img src={instagram} alt="Logo de Instagram"></img>
-              </p>
             </div>
           </div>
-        </div>
-
-        <hr></hr>
-
-        <div className="sb__footer-below">
-          <p>
-            &copy;{new Date().getFullYear()} Napoletana Grosso all right reserved
-          </p>
+          <div className="footer-middle-wrapper">
+            <div className="footer-social-media-block">
+              <div className="footer-title">Seguinos</div>
+              <div className="social-media-icons-wrapper">
+                <a
+                  href=""
+                  className="social-link w-inline-block"
+                  target="_blank"
+                >
+                  <img
+                    src={instagram}
+                    alt="social media icon"
+                    className="social-icon"
+                  />
+                </a>
+                <a
+                  href=""
+                  className="social-link w-inline-block"
+                  target="_blank"
+                >
+                  <img
+                    src={facebook}
+                    alt="social media icon"
+                    className="social-icon"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="footer-bottom-wrapper">
+            <div className="bottom-footer-block left">
+              <div className="footer-text">
+                &copy; {new Date().getFullYear()} Napoletana Grosso
+              </div>
+            </div>
+            <div className="bottom-footer-block center">
+              <a href="" className="footer-link-block w-inline-block">
+                <div className="footer-text">Política de Privacidad</div>
+              </a>
+              <a href="" className="footer-link-block w-inline-block">
+                <div className="footer-text">Términos de uso</div>
+              </a>
+            </div>
+            <div className="bottom-footer-block right">
+              <a href="" className="w-inline-block">
+                <div className="footer-text created">
+                  Website created by A.G.A
+                </div>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
