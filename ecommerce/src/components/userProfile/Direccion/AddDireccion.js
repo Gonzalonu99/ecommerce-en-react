ba047e53228ca2d1a8c6904009cbf6dd52ed7c11
Gonzalo Núñez <gonzalonu99@gmail.com>
2023-12-01 16:49:57 -0300

    fix ortography

--- ecommerce/src/components/userProfile/Direccion/AddDireccion.js
+++ ecommerce/src/components/userProfile/Direccion/AddDireccion.js
@@ -9,7 +9,7 @@ const AddDireccion = () => {
       <Link to={"/mi-perfil/direcciones/agregarNuevaDireccion"} className="btn-add" style={{width:"100%", border:"none", background:"transparent"}}>
         <div className="btn-content">
           <AddIcon sx={{ width: "50px", height: "50px", opacity: ".2" }} />
-          <h3>Agregar direccion</h3>
+          <h3>Agregar dirección</h3>
         </div>
       </Link>
     </div>
